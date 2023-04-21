22549a71fdde5e2973b966f5c944f4123cb14f45
mysoyul <vega2k_@naver.com>
2023-04-21 11:57:11 +0900

    nextjs fullstack

---
+++ app/Posts.tsx
@@ -0,0 +1,35 @@
+import { Post, User } from "@prisma/client";
+import Image from "next/image";
+import Link from "next/link"
+
+interface PostsProps {
+  posts: Array<Post & { author: User }>;
+}
+
+const Posts = ({ posts }: PostsProps) => {
+  return (
+    <>
+      {posts.map((post, idx) => (
+        <div key={idx} className="flex p-3 gap-4 my-3 rounded-xl border-[1px] border-zinc-600 w-3/4">
+          <div>
+            <Image
+              src={post.author.imageUrl || ""}
+              alt="avatar"
+              height={52}
+              width={52}
+              className="rounded-full"
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+          <Link href={`/${post.author.id}`}>
+            <span className="text-xl font-semibold">{post.author.name}</span>
+            </Link>
+            <span className="text-lg font-light">{post.body}</span>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default Posts;
